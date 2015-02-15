54be48483a5ae36a31e7c59f8be9ff64f1819139
satishsarin <satish.sarin@trantorinc.com>
2015-02-15 23:40:00 +0530

    add authenticate_user to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  devise_for :users, :controllers => {registrations: 'registrations'}
 
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
